718d6bd3b38b86c080090a1ddb198ad646aa25b9
Mingkai Zhou <524019351@qq.com>
2019-10-07 11:33:08 +0800

    还是需要一个基类

--- formual.h
+++ formual.h
@@ -5,27 +5,23 @@ enum formType {basic, sqrt, inte, frac, mat};
 
 class maker;
 
-class formual
+class baseFormual
 {
 private:
 	bool verify()
 	{
 		return x1!=-1 && y1!=-1 && fontSize!=-1;
 	}
-	
-	string formStr;
-	
+
 public:
-	int getType() { return basic; }
-	formual(string formStr) : formStr(formStr) {}
+	virtual int getType()=0;
+	virtual void draw()=0;
 	float x1=-1;
 	float y1=-1;
 	int fontSize=-1;
-	void draw();
 	//父节点管理子节点所有权
 	maker* supMaker=nullptr;
 	maker* subMaker=nullptr;
-	
 	~formual()
 	{
 		delete supMaker;
@@ -33,14 +29,25 @@ public:
 	}
 };
 
+class formual : public baseFormual
+{
+private:
+	string formStr;
+	
+public:
+	int getType() { return basic; }
+	formual(string formStr) : formStr(formStr) {}
+	void draw();
+};
+
 class maker
 {
 private:
 	bool makerFlag; //true为上标
-	formual* form;
+	baseFormual* form;
 	
 public:
-	maker(bool markFlag, formual* form) : 
+	maker(bool markFlag, baseFormual* form) : 
 		markFlag(markFlag), form(form) {}
 	void draw()
 	{
@@ -54,10 +61,10 @@ public:
 	}
 };
 
-class sqrtForm : public formual
+class sqrtForm : public baseFormual
 {
 private:
-	formual* form;
+	baseFormual* form;
 	
 public:
 	int getType() { return sqrt; }
@@ -70,16 +77,16 @@ public:
 	}
 };
 
-class inteForm : public formual
+class inteForm : public baseFormual
 {
 private:
-	formual* upper;
-	formual* lower;
-	formual* expr;
+	baseFormual* upper;
+	baseFormual* lower;
+	baseFormual* expr;
 	
 public:
 	int getType() { return inte; }
-	inteForm(formual* upper=nullptr, formual* lower=nullptr, formual* expr=nullptr) : 
+	inteForm(baseFormual* upper=nullptr, baseFormual* lower=nullptr, baseFormual* expr=nullptr) : 
 		upper(upper), lower(lower), expr(expr) {}
 	//这里应该加一个积分/求和积符号的flag
 	void draw();
@@ -91,15 +98,15 @@ public:
 	}
 };
 
-class fracForm : public formual
+class fracForm : public baseFormual
 {
 private:
-	formual* numForm;
-	formual* denForm;
+	baseFormual* numForm;
+	baseFormual* denForm;
 	
 public:
 	int getType() { return frac; }
-	fracForm(formual* numForm, formual* denForm) : numForm(numForm), denForm(denForm) {}
+	fracForm(baseFormual* numForm, baseFormual* denForm) : numForm(numForm), denForm(denForm) {}
 	void draw();
 	~fracForm()
 	{
@@ -108,20 +115,20 @@ public:
 	}
 };
 
-class matForm : public matForm
+class matForm : public baseFormual
 {
 private:
-	list<list<formual*>> allForm;
+	list<list<baseFormual*>> allForm;
 
 public:
 	int getType() { return mat; }
-	matForm(list<list<formual*>> allForm) : allForm(allForm) {}
+	matForm(list<list<baseFormual*>> allForm) : allForm(allForm) {}
 	void draw();
 	~matForm()
 	{
 		for(auto i : allForm)
 		{
-			for(formual* j : i)
+			for(baseFormual* j : i)
 				delete j;
 		}
 	}
